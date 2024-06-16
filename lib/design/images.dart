a6cb6d851092294667652996037568f4bd3ad6ca
Vladislav Adamets <adametsderschopfer@gmail.com>
2024-06-16 23:00:20 +0200

    feat: add state/application images & define in images.dart

--- lib/design/images.dart
+++ lib/design/images.dart
@@ -1,5 +1,32 @@
 import 'package:flutter_svg/flutter_svg.dart';
 
+// @ Application Icons
+final SvgPicture accountCircleImage =
+    SvgPicture.asset('assets/images/application/account_circle.svg');
+final SvgPicture arrowBackImage =
+    SvgPicture.asset('assets/images/application/arrow_back.svg');
+final SvgPicture checkImage =
+    SvgPicture.asset('assets/images/application/check.svg');
+
+// @ State Icons
+final SvgPicture stateBoardingImage =
+    SvgPicture.asset('assets/images/state/boarding.svg');
+final SvgPicture stateDeliveryImage =
+    SvgPicture.asset('assets/images/state/delivery.svg');
+final SvgPicture stateLoadImage =
+    SvgPicture.asset('assets/images/state/load.svg');
+final SvgPicture stateParkingImage =
+    SvgPicture.asset('assets/images/state/parking.svg');
+final SvgPicture statePickupImage =
+    SvgPicture.asset('assets/images/state/pickup.svg');
+final SvgPicture stateRepairImage =
+    SvgPicture.asset('assets/images/state/repair.svg');
+final SvgPicture stateUnBoardingImage =
+    SvgPicture.asset('assets/images/state/unboarding.svg');
+final SvgPicture stateUnloadImage =
+    SvgPicture.asset('assets/images/state/unload.svg');
+
+// @ Vehicle Icons
 final SvgPicture vehicleBusImage =
     SvgPicture.asset('assets/images/vehicles/bus.svg');
 final SvgPicture vehicleCarImage =
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: vehicleBusImage,
+        child: stateBoardingImage,
       ),
     );
   }
